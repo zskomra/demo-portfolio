4737f48ef30f088b176b63c89819ff8097c28be6
z.skomra <z.skomraa@gmail.com>
2020-12-04 14:17:40 +0100

    register user :: saving user

---
+++ src/main/java/projects/portfoliodemo/web/command/RegisterUserCommand.java
@@ -0,0 +1,17 @@
+package projects.portfoliodemo.web.command;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class RegisterUserCommand {
+
+    @NotNull @Email
+    private String username;
+    @NotBlank @Size(min = 4, max = 12)
+    private String password;
+}
